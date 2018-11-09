d7d74e3f9b6a1dee4a09b3b9ebee3e27515f1cfb
likaifeng <1980942486@qq.com>
2018-11-09 19:22:04 +0800

    注册页面用户信息验证完成

--- js/register.js
+++ js/register.js
@@ -1,4 +1,130 @@
 $(function () {
+  init();
+  // 管理函数
+  function init() {
+    // 获取验证码
+    getCode();
+    // 验证注册信息
+    getRegister()
+  }
+
+  // 获取验证码
+  function getCode() {
+    $('#verification').on('tap', function () {
+      // 通过属性选择器获取手机输入框的值
+      var mobilePhone = $('[name="mobile"]').val().trim();
+      // 判断手机号码是否正确
+      if (!$.checkPhone(mobilePhone)) {
+        mui.toast('手机号码格式不正确', {
+          duration: 'long',
+          type: 'div'
+        })
+        return;
+      }
+      // 当手机号码格式正确时,发送请求获取验证码
+      $.post('users/get_reg_code', {
+        mobile: mobilePhone
+      }, function (res) {
+        if (res.meta.status == 200) {
+          $('#verification').attr('disabled', true);
+          var times = 10;
+          // $('#verification').val(times + '后重新获取验证码');
+          var timerId = setInterval(function () {
+            times--;
+            $('#verification').html(times + '秒后重新获取验证码');
+            if (times <= 0) {
+              $('#verification').removeAttr('disabled');
+              clearInterval(timerId);
+              $('#verification').html('获取验证码');
+            }
+            console.log(times);
+          }, 500)
+          mui.toast('验证码为 : ' + res.data, {
+            duration: 'long',
+            type: 'div'
+          })
+        }
+      })
+    })
+  }
+
+
+  // 验证注册信息
+  function getRegister() {
+    $('#regBtn').on('tap', function () {
+      // 获取注册信息
+      var mobile = $('[name="mobile"]').val().trim();
+      var code = $('[name="code"]').val().trim();
+      var email = $('[name="email"]').val().trim();
+      var pwd = $('[name="password"]').val().trim();
+      var pwd_con = $('[name="password_confirm"]').val().trim();
+      var gender = $('[name="gender"]').val();
+      // 验证手机号码
+      if (!$.checkPhone(mobile)) {
+        mui.toast('手机号码格式不正确', {
+          duration: 'long',
+          type: 'div'
+        })
+        return;
+      }
+      // 验证验证码
+      if (code.length != 4) {
+        mui.toast('请输入4位验证码', {
+          duration: 'long',
+          type: 'div'
+        })
+        return;
+      }
+      // 验证邮箱
+      if (!$.checkEmail(email)) {
+        mui.toast('邮箱格式不正确', {
+          duration: 'long',
+          type: 'div'
+        })
+        return;
+      }
+      // 验证密码
+      if (pwd.length < 6) {
+        mui.toast('请输入6位密码', {
+          duration: 'long',
+          type: 'div'
+        })
+        return;
+      }
+      // 验证确认密码和密码是否一致
+      if (pwd_con != pwd) {
+        mui.toast('两次密码不一致', {
+          duration: 'long',
+          type: 'div'
+        })
+        return;
+      }
+      // 当全部信息验证通过时,发送请求
+      var userRegister = {
+        mobile: mobile,
+        code: code,
+        email: email,
+        pwd: pwd,
+        gender: gender
+      }
+      $.post('users/reg', userRegister, function (res) {
+        // console.log(res);
+      })
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
   // 定义全局变量
   // var dataCode;
   // var telphone;
