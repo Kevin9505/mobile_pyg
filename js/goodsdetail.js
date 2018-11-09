8342e4bc03d6e1a51292939502d64461d50dea6b
likaifeng <1980942486@qq.com>
2018-11-09 20:21:19 +0800

    完成了成功添加到购物车的功能(用$.ajax()发送请求,因为当前的接口必须在请求头里,添加token,$.post()没有办法添加;data:只是放正常的业务流程的参数  headers: 一般是存放 登录 凭证相关;根据本地存储获取用户的钥匙token,放到请求头中一起发送请求);数据成功返回后,判断是否跳转到购物车页面

--- js/goodsdetail.js
+++ js/goodsdetail.js
@@ -1,4 +1,6 @@
 $(function () {
+  // 定义全局变量 全局变量首字母大写
+  var GoodsInfo;
   init()
 
   function init() {
@@ -18,6 +20,7 @@ $(function () {
     }, function (res) {
       if (res.meta.status == 200) {
         console.log(res);
+        GoodsInfo = res.data;
         var html = template('goodsDetailTpl', {
           data: res.data
         });
@@ -39,9 +42,9 @@ $(function () {
   function addCart() {
     $('.pyg_addCart').on('tap', function () {
       // 点击加入购物车后,先判断是否已经登录了,从本地存储中获取信息,从而判断是否登录
-      var userInfo = localStorage.getItem('userInfo');
+      var userInfoStr = localStorage.getItem('userInfo');
       // console.log(userInfo);
-      if (!userInfo) {
+      if (!userInfoStr) {
         mui.toast('请先登录', {
           duration: 'long',
           type: 'div'
@@ -50,6 +53,48 @@ $(function () {
           location.href = './login.html';
         }, 1000)
         return;
+      } else {
+        // 登录了,获取当前商品的信息
+        var info = {
+          cat_id: GoodsInfo.cat_id,
+          goods_id: GoodsInfo.goods_id,
+          goods_name: GoodsInfo.goods_name,
+          goods_number: GoodsInfo.goods_number,
+          goods_price: GoodsInfo.goods_price,
+          goods_small_logo: GoodsInfo.goods_small_logo,
+          goods_weight: GoodsInfo.goods_weight
+        }
+        // console.log(info);
+        // 发送请求
+        var infoStr = JSON.stringify(info);
+        var userToken = JSON.parse(userInfoStr).token;
+        // console.log(userToken);
+        /**因为当前的接口必须在请求头里,添加token,$.post()没有办法添加;data:只是放正常的业务流程的参数  headers: 一般是存放 登录 凭证相关 */
+        $.ajax({
+          type: 'post',
+          url: 'my/cart/add',
+          data: {
+            info: infoStr
+          },
+          Authorization: userToken,
+          success: function (res) {
+            console.log(res);
+            if (res.meta.status == 200) {
+              mui.confirm('添加成功，跳转到购物车页面?', '温馨提示', ['跳转', '取消'], function (type) {
+                if (type.index == 0) {
+                  location.href = './shoppingCart.html'
+                } else {
+                  console.log('取消');
+                }
+              })
+            } else {
+              mui.toast(res.meta.status, {
+                duration: 'long',
+                type: 'div'
+              })
+            }
+          }
+        })
       }
     })
   }
