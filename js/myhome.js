9520ce4be04508eca7ce0f05db504515cb1d170c
likaifeng <1980942486@qq.com>
2018-11-11 11:41:50 +0800

    个人主页数据的渲染,退出登录功能的实现

--- js/myhome.js
+++ js/myhome.js
@@ -12,6 +12,33 @@ $(function () {
       return;
     }
     $('body').fadeIn();
+
+    getUserData()
+    eventList()
+  }
+
+  // 获取用户信息
+  function getUserData() {
+    $.get('my/users/userinfo', function (res) {
+      console.log(res);
+      if (res.meta.status == 200) {
+        $('.userName').text(res.data.user_tel);
+        $('.mui-ellipsis').text(res.data.user_email)
+      }
+    })
+  }
+
+  // 退出登录
+  function eventList() {
+    $('.logout a').on('tap', function () {
+      // console.log(1);
+      mui.confirm('您确定要退出登录吗?', '温馨提醒', ['取消', '确认'], function (etype) {
+        if (etype.index == 1) {
+          localStorage.clear();
+          location.href = './login.html'
+        }
+      });
+    })
   }
 
 
